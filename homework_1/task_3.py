721827e4f98443a21cada68d601ff5b2ff4352ee
Dmitriy <BorisovDm@users.noreply.github.com>
2019-07-25 15:14:40 +0300

    [HW-1] update task 3

--- homework_1/task_3.py
+++ homework_1/task_3.py
@@ -1,40 +1,48 @@
-from confluent_kafka import Consumer, KafkaException
 from multiprocessing import Pool
 
+from confluent_kafka import Consumer, KafkaException
 
+
+BOOTSTRAP_SERVERS = '10.156.0.3:6667,10.156.0.4:6667,10.156.0.5:6667'
+GROUP_ID = 'dborisov_hw1_task3'
 TOPIC_NAME = 'mles.announcements'
 
 
-def get_consumer(is_subscribe=True):
+def get_consumer():
     consumer = Consumer({
-        'bootstrap.servers': '10.156.0.3:6667,10.156.0.4:6667,10.156.0.5:6667',
-        'group.id': 'dborisov_hw1_task3',
-    })
-    if is_subscribe:
-        consumer.subscribe([TOPIC_NAME]) 
+        'bootstrap.servers': BOOTSTRAP_SERVERS,
+        'group.id': GROUP_ID,
+    }) 
     return consumer
 
 
-def consume_data(_):
+def consume_data(partition_number):
     consumer = get_consumer()
+    consumer.subscribe([TOPIC_NAME])
+
+    output_file = open(f'{TOPIC_NAME}_{partition_number}', 'a')
+
     try:
         while True:
-            msg = consumer.poll(timeout=1.0)
+            msg = consumer.poll(timeout=0)
             if msg is None:
                 continue
             if msg.error():
                 raise KafkaException(msg.error())
-            with open(f'{msg.topic()}_{msg.partition()}', 'a') as f:
-                f.write(f'{msg.value().decode("utf-8")}\n')
+            output_file.write(f'{msg.value().decode("utf-8")}\n')
     except KeyboardInterrupt:
         pass
     finally:
+        output_file.close()
         consumer.close()
 
 
 if __name__ == '__main__':
-    consumer = get_consumer(is_subscribe=False)
+    consumer = get_consumer()
     n_partitions = len(consumer.list_topics().topics[TOPIC_NAME].partitions)
 
     with Pool(processes=n_partitions) as pool:            
-        pool.map(consume_data, range(n_partitions))
+        try:    
+            pool.map(consume_data, range(n_partitions))
+        except KeyboardInterrupt:
+            pool.terminate()
